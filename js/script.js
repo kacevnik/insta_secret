1c475b261a8bf213c8e69431261f651f2fbebb10
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-01-22 18:18:28 +0300

    Закончена верстка главной старницы Signed-off-by: Dmitrii Kovalev <kacevnik@yandex.ru>

--- js/script.js
+++ js/script.js
@@ -1,3 +1,11 @@
 $( document ).ready(function() {
-	
+
+	$('.item_hide').hover(function(){
+		$('.item_hide').not(this).slideDown(400);
+		$(this).slideUp(400);
+	});
+
+	$('.container').hover(function(){
+		$('.item_hide').slideDown(400);
+	});
 });
