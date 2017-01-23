6f25a4273ba7a579d94a2b5895861dd635625fd2
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-01-23 10:39:29 +0300

    Адаптивная верстка Signed-off-by: Dmitrii Kovalev <kacevnik@yandex.ru>

--- js/script.js
+++ js/script.js
@@ -8,4 +8,28 @@ $( document ).ready(function() {
 	$('.container').hover(function(){
 		$('.item_hide').slideDown(400);
 	});
+
+	$('.menu_tap').click(function(){
+		$(this).hide();
+		$('.close_tap').show();
+		$('.hide_search').slideUp('slow');
+		$('.hide_menu').slideDown('slow');
+	});
+
+	$('.close_tap').click(function(){
+		$(this).hide();
+		$('.menu_tap').show();
+		$('.hide_menu').slideUp('slow');
+	});
+
+	$('.search_tap').click(function(){
+		if($('.hide_search').css('display') == 'block'){
+			$('.hide_search').slideUp('slow');
+		}else{
+			$('.menu_tap').show();
+			$('.close_tap').hide();
+			$('.hide_menu').slideUp('slow');
+			$('.hide_search').slideDown('slow');
+		}
+	});
 });
