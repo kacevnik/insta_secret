0b72e4369dcc0ece23f4fb994244f750d35f34bb
Dmitrii Kovalev <kacevnik@yandex.ru>
2017-03-07 14:08:11 +0300

    Корректировка верстки футера

--- footer.php
+++ footer.php
@@ -1,27 +1,110 @@
 <?php
 /**
  * Шаблон подвала (footer.php)
- * @package WordPress
- * @subpackage your-clean-template-3
  */
 ?>
-	<footer>
-		<div class="container">
-			<div class="row">
-				<div class="col-md-12">
-					<?php $args = array( // опции для вывода нижнего меню, чтобы они работали, меню должно быть создано в админке
-						'theme_location' => 'bottom', // идентификатор меню, определен в register_nav_menus() в function.php
-						'container'=> false, // обертка списка, false - это ничего
-						'menu_class' => 'nav nav-pills bottom-menu', // класс для ul
-				  		'menu_id' => 'bottom-nav', // id для ul
-				  		'fallback_cb' => false
-				  	);
-					wp_nav_menu($args); // выводим нижние меню
-					?>
-				</div>
-			</div>
-		</div>
-	</footer>
-<?php wp_footer(); // необходимо для работы плагинов и функционала  ?>
+<script type="text/javascript">
+            //Apply javascript to new posts being loaded in by infinite scroll
+            function titlePadding(){
+                $videoHeight = $('.box-sm > img').height();
+                $containerHeight = $('.box-content > h4').height();
+                $backgroundTop = $videoHeight - $containerHeight;
+                $backgroundTop = $backgroundTop - 120;
+                $('.box-content > h4').css('padding-top', $backgroundTop);
+                $('.blog-item').hover(function(){
+                    $backgroundTop = 0;
+                    if($backgroundTop < 0){
+                        $backgroundTop = 0;
+                    }
+                    $(".box-content > h4", this).css('padding-top', $backgroundTop);
+                }, function(){
+                    //Reset backgroundTop value to prevent stacking negatives
+                    $videoHeight = $('.box-sm > img').height();
+                    $containerHeight = $('.box-content > h4').height();
+                    $backgroundTop = $videoHeight - $containerHeight;
+                    $backgroundTop = $backgroundTop - 120;
+                    $(".box-content > h4", this).css('padding-top', $backgroundTop); 
+                });
+            };
+            $('.blog-item').hover(function(){
+                $backgroundTop = 0
+                if($backgroundTop < 0){
+                    $backgroundTop = 0;
+                }
+                $(".box-content > h4", this).css('padding-top', $backgroundTop);
+            }, function(){
+                $videoHeight = $('.box-sm > img').height();
+                $containerHeight = $('.box-content > h4').height();
+                $backgroundTop = $videoHeight - $containerHeight;
+                $backgroundTop = $backgroundTop - 120;
+                $(".box-content > h4", this).css('padding-top', $backgroundTop); 
+            });
+            $(document).ready(function(){
+                //Set h4 height and hover state for blog posts
+                $videoHeight = $('.box-sm > img').height();
+                $containerHeight = $('.box-content > h4').height();
+                $backgroundTop = $videoHeight - $containerHeight;
+                $backgroundTop = $backgroundTop - 120;
+                $('.box-content > h4').css('padding-top', $backgroundTop);
+                $('.blog-item').hover(function(){
+                $backgroundTop = 0;
+                if($backgroundTop < 0){
+                    $backgroundTop = 0;
+                }
+                $(".box-content > h4", this).css('padding-top', $backgroundTop);
+                }, function(){
+                    $videoHeight = $('.box-sm > img').height();
+                    $containerHeight = $('.box-content > h4').height();
+                    $backgroundTop = $videoHeight - $containerHeight;
+                    $backgroundTop = $backgroundTop - 120;
+                    $(".box-content > h4", this).css('padding-top', $backgroundTop); 
+                });
+
+            })  ;         
+    
+    
+            $(window).resize(function(){
+                $videoHeight = $('.box-sm > img').height();
+                $containerHeight = $('.box-content > h4').height();
+                $backgroundTop = $videoHeight - $containerHeight;
+                $backgroundTop = $backgroundTop - 120;
+                $('.box-content > h4').css('padding-top', $backgroundTop);
+            });
+    
+</script>
+  <div id="footer-wrapper">
+    <footer id="sub-footer">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col-md-12 center">
+
+            <article class="col-md-5 center">
+              <div class="widget">
+                <h4>Свяжитесь с нами</h4>
+                <ul class="socialmedia">
+                  <li><a href="" target="_blank"><i class="fa fa-facebook"></i></a>
+                  </li>
+                  <li><a href="" target="_blank"><i class="fa fa-twitter"></i></a></li>
+                  <li><a href="" target="_blank"><i class="fa fa-linkedin"></i></a>
+                  </li>
+                  <li><a href="" target="_blank"><i
+                          class="fa fa-google-plus"></i></a></li>
+                  <li><a href="" target="_blank"><i
+                          class="fa fa-youtube"></i></a></li>
+                  <li><a href="/" target="_blank"><i class="fa fa-rss-square"></i></a></li>
+                </ul>
+              </div>
+            </article>
+
+          </div>
+        </div>
+      </div>
+    </footer>
+
+    <div class="footer-bar clearfix">
+        <p class="left">&copy; <?php echo date("Y")." "; bloginfo( $string ); ?>. Все права защещены.</p>
+    </div>
+  </div>
+
 </body>
 </html>
